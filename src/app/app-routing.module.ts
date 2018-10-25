ebbb3868a624956d553d5cac677c9bc1facdb450
IllichG <illich.gaspar@augensolutionit.com>
2018-10-25 10:59:36 -0500

    hola

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+
+import { Routes } from '@angular/router'
+
+const app_routes: Routes = [
+    { path: '', component:  }
+
+];
+
+@NgModule({
+
+})
+
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,25 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { MenuComponent } from './shared/menu/menu.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { DeptosComponent } from './pages/deptos/deptos.component';
+import { InitComponent } from './pages/init/init.component';
+import { TareasComponent } from './pages/tareas/tareas.component';
+import { PropsComponent } from './pages/props/props.component';
+import { CalidadComponent } from './pages/calidad/calidad.component';
+import { ComentsComponent } from './pages/coments/coments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MenuComponent,
-    FooterComponent
+    FooterComponent,
+    DeptosComponent,
+    InitComponent,
+    TareasComponent,
+    PropsComponent,
+    CalidadComponent,
+    ComentsComponent
   ],
   imports: [
     BrowserModule
